0ab8c21ed6eab3aca5f02116556b34551692891c
Charlie Brown <charlieb@factitious2.game>
2017-05-05 17:10:46 -0400

    set django debug to false

--- factitious/settings.py
+++ factitious/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4i@q&)^@!zqkzfdvmy*2$v)+4#z$&eoz@ckzb#-yml&(3@#5^p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
